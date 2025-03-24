d4d42ea32575155a303bd575def2453ac06d7695
Cesar <let5@newpaltz.edu>
2025-03-24 16:36:05 -0400

    update controller and tools folders

--- server/index.js
+++ server/index.js
@@ -1,63 +1,25 @@
-
-/*
-var http = require('http');
-
-const PORT = 3000; 
-
-
-/*const server = http.createServer((request, response) => {
-    response.statusCode = 200, {'Content-Type': 'text/plain'};
-    response.end('Hello, World!\n');
-});*/
-/*same thing 
-const server = http.createServer(function handler(request, response){
-    response.statusCode = 200, {'Content-Type': 'text/plain'};
-    response.end('Hello, World!\n');
-});
-
-
-const square = (x) => x * x;  /** Arrow function with parameter x and doesn't have to have return */
-/*const square(x)=>{
-    return x * x; 
-}*/
-
-
-
-// Start the server and listen on the specified port
-/*server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
-
-
-console.log("Hello, World!");*/
-
-
-
-
-
-
 /*  B"H
 */
 // Load the http module to create an http server.
-const http = require('http')
+const express = require('express')
+const productsController = require('./controllers/products')
+
 const PORT = 8000
 
-//const app= express();
+const app = express();
 
-
-
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World\n');
-});
+app
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz, NY!!!')
+  })
+  .use('/products', productsController)
 
 // Listen on port 8000, IP defaults to
 //
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`)
 });
 
 
 
-console.log('Hello World!');
+console.log('Hello World!')
